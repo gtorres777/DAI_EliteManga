cae7bfb9de0c0ef28632d95ae5fede610e7c6409
gtorres777 <gustavo.torres.d@tecsup.edu.pe>
2019-06-10 02:24:27 -0500

    vers4

--- app/Producto.php
+++ app/Producto.php
@@ -9,4 +9,14 @@ class Producto extends Moloquent
     protected $fillable = [
         'nombre_producto', 'precio', 'stock', 'caracteristicas'
     ];
+
+    public function users()
+    {
+       return $this->belongsTo('App\User');
+    }
+
+    public function compras()
+    {
+       return $this->belongsTo('App\Compra');
+    }
 }
