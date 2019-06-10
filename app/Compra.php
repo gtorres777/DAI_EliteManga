a91c50be4f4c5118c1713e803de57ed5beb59490
unknown <gustavo.torres.d@tecsup.edu.pe>
2019-06-09 23:44:25 -0500

    version3

--- app/Compra.php
+++ app/Compra.php
@@ -7,6 +7,21 @@ use Jenssegers\Mongodb\Eloquent\Model as Moloquent;
 class Compra extends Moloquent
 {
     protected $fillable = [
-        'usuario_id', 'fecha_compra'
+        'fecha_compra'
     ];
+
+    public function entrega ()
+    {
+        return $this->belongsTo('App\Entrega');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function productos()
+    {
+        return $this->embedsMany('App\Producto');
+    }
 }
