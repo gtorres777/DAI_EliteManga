0e67e1e64389837ba7968fe131eb0024f31e75a7
edgardo3000 <jean.delgado@tecsup.edu.pe>
2019-06-10 02:18:47 -0500

    vers4

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
-        $this->call(UsuarioCollectionSeeder::class);
+        $this->call(UsersCollectionSeeder::class);
         $this->call(ProductosCollectionSeeder::class);
         $this->call(MangasCollectionSeeder::class);
     }
