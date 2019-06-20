35c282908b3dc9a2d71b4ff1c58f841b4b66a9d4
gtorres777 <gustavo.torres.d@tecsup.edu.pe>
2019-06-20 14:47:15 -0500

    versionf

--- resources/views/agregarproductos.blade.php
+++ resources/views/agregarproductos.blade.php
@@ -2,8 +2,9 @@
 
 @section('content')
     <div class="container">
-	<div class="row justify-content-center">
-			<h2>Nuevo Producto</h2>
+	<div class="row card mt-5 text-white" style="background-color:rgba(31,31,31, 0.5)">
+		<div class="col-12 col-sm-12 col-md-12 col-xl-12 pb-2">
+			<h2 class="textbox">Nuevo Producto</h2>
 	</div>
 	<div class="row justify-content-center mt-3">
 	@if ($message = Session::get('success'))
@@ -24,12 +25,12 @@
 		</div>
 	@endif
 
-	<form action="{{action ('ProductoController@store')}}" method="POST" enctype="multipart/form-data">
+	<form action="{{action ('ProductoController@store')}}" method="POST" enctype="multipart/form-data" class="col-12">
 	@csrf
 		<div class="form-group row">
 			<label for="title" class="col-sm-4 col-form-label text-md-right">{{ __('Nombre_producto') }}</label>
 			<div class="col-md-6">
-				<input id="nombre_producto" type="text" class="form-control{{ $errors->has('nombre_producto') ? ' is-invalid' : '' }}" name="nombre_producto" value="{{ old('nombre_producto') }}" autofocus />
+				<input id="nombre_producto" type="text" class="bg-white text-dark form-control{{ $errors->has('nombre_producto') ? ' is-invalid' : '' }}" name="nombre_producto" value="{{ old('nombre_producto') }}" autofocus />
 				@if ($errors->has('numero_capitulo'))
 					<span class="invalid-feedback" role="alert">
 						<strong>{{ $errors->first('nombre_producto')}}</strong>
@@ -40,7 +41,7 @@
 		<div class="form-group row">
 			<label for="title" class="col-sm-4 col-form-label text-md-right">{{ __('Caracteristicas') }}</label>
 			<div class="col-md-6">
-				<input id="caracteristicas" type="text" class="form-control{{ $errors->has('caracteristicas') ? ' is-invalid' : '' }}" name="caracteristicas" value="{{ old('caracteristicas') }}" autofocus />
+				<input id="caracteristicas" type="text" class="bg-white text-dark form-control{{ $errors->has('caracteristicas') ? ' is-invalid' : '' }}" name="caracteristicas" value="{{ old('caracteristicas') }}" autofocus />
 				@if ($errors->has('caracteristicas'))
 					<span class="invalid-feedback" role="alert">
 						<strong>{{ $errors->first('caracteristicas')}}</strong>
@@ -51,7 +52,7 @@
         <div class="form-group row">
 			<label for="title" class="col-sm-4 col-form-label text-md-right">{{ __('Precio') }}</label>
 			<div class="col-md-6">
-				<input id="precio" type="number" class="form-control{{ $errors->has('precio') ? ' is-invalid' : '' }}" name="precio" value="{{ old('precio') }}" autofocus />
+				<input id="precio" type="number" class="bg-white text-dark form-control{{ $errors->has('precio') ? ' is-invalid' : '' }}" name="precio" value="{{ old('precio') }}" autofocus />
 				@if ($errors->has('precio'))
 					<span class="invalid-feedback" role="alert">
 						<strong>{{ $errors->first('precio')}}</strong>
@@ -62,7 +63,7 @@
         <div class="form-group row">
 			<label for="title" class="col-sm-4 col-form-label text-md-right">{{ __('Stock') }}</label>
 			<div class="col-md-6">
-				<input id="stock" type="number" class="form-control{{ $errors->has('stock') ? ' is-invalid' : '' }}" name="stock" value="{{ old('stock') }}" autofocus />
+				<input id="stock" type="number" class="bg-white text-dark form-control{{ $errors->has('stock') ? ' is-invalid' : '' }}" name="stock" value="{{ old('stock') }}" autofocus />
 				@if ($errors->has('stock'))
 					<span class="invalid-feedback" role="alert">
 						<strong>{{ $errors->first('stock')}}</strong>
@@ -73,7 +74,7 @@
 		<div class="form-group row">
 			<label for="price" class="col-md-4 col-form-label text-md-right">{{ __('Imagen del producto') }}</label>
 			<div class="col-md-6">
-				<input id="imgProducto" type="file" class="form-control-file{{ $errors->has('imgProducto') ? ' is-invalid' : '' }}" name="imgProducto" >
+				<input id="imgProducto" type="file" class="pl-2 pt-2 form-control-file{{ $errors->has('imgProducto') ? ' is-invalid' : '' }}" name="imgProducto" >
 				@if ($errors->has('imgProducto'))
 					<span class="invalid-feedback" role="alert">
 						<strong>{{ $errors->first('imgProducto')}}</strong>
@@ -81,10 +82,10 @@
 				@endif
 			</div>
 		</div>
-		<div class="form-group row mb-0">
-			<div class="col-md-8 offset-md-4">
+		<div class="form-group row mb-4">
+			<div class="col-md-8 offset-md-5">
 				<button type="submit" class="btn btn-primary">
-					{{ __('Guardar') }}
+					{{ __('Guardar Producto') }}
 				</button>
 			</div>
 		</div>
